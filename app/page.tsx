b05abb19ff6b5bb6a34622af2abb19cbcab93618
Chris Gervang <chris.gervang@joby.aero>
2023-01-11 22:11:29 -0800

    Move bar examples to vault

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
 
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer bg-brown-600 p-2 text-white rounded-md">
           <h1>
-            {mostRecentBean.name}
+            <Balancer>{mostRecentBean.name}</Balancer>
           </h1>
 
           {mostRecentBean.subtitle ? (
